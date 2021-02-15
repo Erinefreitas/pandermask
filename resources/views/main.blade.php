8af7cac62cc8793e63bcdb537511d9fb03231ed2
Erine <santos.erine@academico.ifg.edu.br>
2021-02-15 19:42:15 -0300

    Autenticação

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -247,7 +247,7 @@
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{\Illuminate\Support\Facades\Auth::user()->name}}</span>
                             <img class="img-profile rounded-circle"
                                  src="{{asset('img/undraw_profile.svg')}}">
                         </a>
@@ -326,7 +326,10 @@
             <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a>
+                <form method="post" action="{{route('logout')}}">
+                    <a class="btn btn-primary" onclick="event.preventDefault();
+                        this.closest('form').submit();">Logout</a>
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,38 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    $produtos = \App\Models\Produto::all();
-    $categorias = \App\Models\Categoria::all();
-	return view('vitrine', compact('produtos', 'categorias'));
-});
+Route::get('/dashboard', function () {
+    return redirect()->route('clientes.index');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('categoriasvitrine/{categoria}', function ($categoria){
-    $categorias = \App\Models\Categoria::all();
-    $categoriaview = \App\Models\Categoria::findOrFail($categoria);
-    return view('vitrine2', compact('categorias', 'categoriaview'));
-})->name('categorias.vitrine');
+Route::resource('clientes',\App\Http\Controllers\ClienteController::class)->middleware(['auth']);
+Route::resource('categorias',\App\Http\Controllers\CategoriaController::class)->middleware(['auth']);
+Route::resource('produtos',\App\Http\Controllers\ProdutoController::class)->middleware(['auth']);
 
-Route::get('user/{id}', function ($id){
-    return "User: " . $id;
-});
-
-Route::resource('clientes', \App\Http\Controllers\ClienteController::class);
-Route::resource('categorias', \App\Http\Controllers\CategoriaController::class);
-Route::resource('produtos', \App\Http\Controllers\ProdutoController::class);
-
+require __DIR__.'/auth.php';
